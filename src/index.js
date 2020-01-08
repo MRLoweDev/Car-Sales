68fb35a0359c916a83e7d2a3cdd4041a57b6898c
Michael Lowe <mrlowememphis@yahoo.com>
2020-01-08 16:27:00 -0600

    Reducer added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+
+const store = createStore(carSalesReducer);
+
+
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
